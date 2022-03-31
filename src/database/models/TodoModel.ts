4ecf3331cf54b0e057c0f14b051e7348db820ed7
leoprietsch <leo.prietsch@gmail.com>
2022-03-31 20:48:08 -0300

    feat: define Todo model with sequelize

---
+++ src/database/models/TodoModel.ts
@@ -0,0 +1,25 @@
+import { DataTypes } from "sequelize/types";
+import { db } from "../db";
+
+export const TodoModel = db.define("todo", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  priority: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 3,
+  },
+  done: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+});
