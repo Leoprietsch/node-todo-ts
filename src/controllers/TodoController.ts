6ee9f67e49ae5b8687451c85060dda6e43a737e8
leoprietsch <leo.prietsch@gmail.com>
2022-04-01 10:07:25 -0300

    feat: implement todo CRUD controller

--- src/controllers/TodoController.ts
+++ src/controllers/TodoController.ts
@@ -1,15 +1,46 @@
 import { Request, Response } from "express";
+import { TodoModel } from "../database/models/TodoModel";
 
 class TodoController {
-  async findAll(req: Request, res: Response) {}
+  async findAll(req: Request, res: Response) {
+    const todos = await TodoModel.findAll();
 
-  async findById(req: Request, res: Response) {}
+    return todos ? res.status(200).json(todos) : res.sendStatus(204);
+  }
 
-  async create(req: Request, res: Response) {}
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
 
-  async update(req: Request, res: Response) {}
+    const todo = await TodoModel.findOne({ where: { id: id } });
 
-  async delete(req: Request, res: Response) {}
+    return todo ? res.status(200).json(todo) : res.sendStatus(204);
+  }
+
+  async create(req: Request, res: Response) {
+    const { text, priority } = req.body;
+    const todo = await TodoModel.create({
+      text,
+      priority,
+    });
+
+    return res.status(201).json(todo);
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const success = await TodoModel.update(req.body, { where: { id: id } });
+
+    return success ? res.sendStatus(204) : res.sendStatus(404);
+  }
+
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const success = await TodoModel.destroy({ where: { id: id } });
+
+    return success ? res.sendStatus(204) : res.sendStatus(404);
+  }
 }
 
 export default new TodoController();
