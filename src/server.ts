37d8be9566368c4935ca61a840ecf55225067227
leoprietsch <leo.prietsch@gmail.com>
2022-04-01 10:05:34 -0300

    fix: remove cors

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,16 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors);
 app.use(morgan("dev"));
 
 app.use(router);
 
-const server = app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(
     `${process.env.PROJECT_NAME} running at http://localhost:${process.env.PORT}`
   );
-});
-
-process.on("SIGINT", () => {
-  server.close();
-  console.log(`${process.env.PROJECT_NAME} is shuting down...`);
 });
