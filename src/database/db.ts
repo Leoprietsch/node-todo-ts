6231602951137d9ae4d89ce6280d6974bb0e74f6
leoprietsch <leo.prietsch@gmail.com>
2022-04-05 10:10:49 -0300

    feat(database): create database schema if it doesn't exist

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,4 +1,20 @@
+import mysql from "mysql2/promise";
 import { Sequelize } from "sequelize";
+
+initialize();
+
+async function initialize() {
+  const connection = await mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  });
+
+  await connection.query(
+    `CREATE DATABASE IF NOT EXISTS ${process.env.DATABASE_NAME};`
+  );
+}
 
 export const db = new Sequelize(
   process.env.DATABASE_NAME,
